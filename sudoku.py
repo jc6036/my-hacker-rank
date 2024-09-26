b1eff8039b7e4192ea96cccda19ddae4f1e19294
Jesse Cabell <jc6036@gmail.com>
2024-09-26 16:59:46 -0400

    Update sudoku.py - todays adjustments

--- sudoku.py
+++ sudoku.py
@@ -9,28 +9,13 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # Solution
-        # 1. Go to empty cells, row-by-row
-        # 2. Attempt to solve that cell by checking if there are any valid numbers for that cell
-        #   2a. Check every number in the submatrix, line, and row. Add them to a unique list, no dupes
-        #   3a. If there is a number >=1 and <=9 that is NOT present in the array, that is the answer
-        # 3. Proceed through each empty square, repeating from the start, until completely solved
-        # Optimizations
-        # 1. Weight each empty cell with the sum of digits already figured for it - return to cells with
-        #    the highest weights first
-        #    1a. Can be accomplished with a supplementary structure of map of pairs. [(X,Y), Z]        
-        # 2. Check between every quad/row/column for a valid number, to skip some calcluations
-        # 3. Only re-calculate row, quadrant, or column, if the next square you choose is in a different one
-        # 4. Issue: can't solve when therea re squares with more than one potential answer. Need to implement
-        #    memory for each empty square, so that I can reference all the possible solutions for each square
-        #    and adjust them as I go. If a square has a possible answer that no other square in a row, column, or sub has, then that's the answer.
 
         solved_flag = False
         prev_x = 0
         prev_y = 0
-        possibles = {}
         while solved_flag is False:
             # Collect all possible answers
+            possibles = {}
             for y in range(9):
                 for x in range(9):
                     if board[y][x] == ".":
@@ -39,15 +24,9 @@ class Solution:
                     prev_x = x
                     prev_y = y
 
-            # Now do a pass checking for simple solves
-            # If there's only one possible answer, just put it into the board
-            for k, v in possibles.items():
-                if len(v) == 1:
-                    board[k[0]][k[1]] = v
-
-            # Now check for more complicated solves using a search
-            
-
+            # Now analyze each square and perform a process of elimination on possibles for that
+            # square. If you get down to 1 possible left, that's the answer.
+            board = self.solvePossibles(board, possibles)
 
             # Check to see if solved yet
             solved_flag = True
@@ -78,8 +57,8 @@ class Solution:
 
         possibles = []
         for k, v in unique_numbers.items():
-            if flag == False:  # Aka, if this number isn't already found in the groupings, it's possible
-                possibles.insert(v)
+            if v == False:  # Aka, if this number isn't already found in the groupings, it's possible
+                possibles.append(k)
 
         return possibles
     
@@ -114,16 +93,16 @@ class Solution:
         elif x >= 3 and x<= 5:
             if y >= 0 and y <= 2:
                 numbers = self.searchSub(board, 3, 0)
-            elif y >= 3 and y <= 5:  # Center
+            elif y >= 3 and y <= 5: 
                 numbers = self.searchSub(board, 3, 3)
-            elif y >= 6 and y <= 8:  # Bottom Center
+            elif y >= 6 and y <= 8: 
                 numbers = self.searchSub(board, 3, 6)
-        elif x >= 6 and x <= 8:  # Right Side
-            if y>= 0 and y <= 2:  # Top Right
+        elif x >= 6 and x <= 8: 
+            if y>= 0 and y <= 2: 
                 numbers = self.searchSub(board, 6, 0)
-            elif y >= 3 and y <= 5:  # Middle Right
+            elif y >= 3 and y <= 5:
                 numbers = self.searchSub(board, 6, 3)
-            elif y >= 6 and y <= 8:  # Bottom Right
+            elif y >= 6 and y <= 8:
                 numbers = self.searchSub(board, 6, 6)
 
         return numbers
@@ -138,8 +117,87 @@ class Solution:
         
         return numbers
     
-    def checkComplexSolves(self, board: [List[List[str]]], possibles):
-        # Build a new list of possibles for the cell through process of elimination
-        # If any other cell in a group has the same possible, we eliminate it (A bit of a shortcut)
-        # If we are left with one possible, that's the answer
+    def solvePossibles(self, board: [List[List[str]]], possibles):
+        for y in range(9):
+            for x in range(9):
+                if board[y][x] == ".":
+                    tracker = possibles[(y,x)].copy()
+
+                    tracker = self.solveGroups(board, tracker, possibles, x, y)
+
+                    if len(tracker) == 1:
+                        board[y][x] = tracker[0]
+                        possibles.pop((y,x))
+                    
+        return board
+    
+    def solveGroups(self, board: [List[List[str]]], tracker, possibles, x, y):
+        if len(tracker) > 0:
+            tracker = self.solveRows(board, tracker, possibles, y, x)
         
+        if len(tracker) > 0:
+            tracker = self.solveColumns(board, tracker, possibles, x, y)
+        
+        if len(tracker) > 0:
+            tracker = self.solveSubMatrix(board, tracker, possibles, x, y)
+
+        return tracker
+    
+    def solveRows(self, board: [List[List[str]]], tracker, possibles, y, x):
+        for i in range(9):
+            if board[y][i] == "." and i != x:
+                for v in possibles[(y,i)]:
+                    for z in range(len(tracker)):
+                        if v == tracker[z]:
+                            tracker.pop(z)
+                            break
+        
+        return tracker
+    
+    def solveColumns(self, board: [List[List[str]]], tracker, possibles, x, y):
+        for i in range(9):
+            if board[i][x] == "." and i != y:
+                for v in possibles[(i,x)]:
+                    for z in range(len(tracker)):
+                        if v == tracker[z]:
+                            tracker.pop(z)
+                            break
+        
+        return tracker
+    
+    def solveSubMatrix(self, board, tracker, possibles, x, y):
+        if x >= 0 and x <= 2: 
+            if y >= 0 and y <= 2: 
+                tracker = self.solveSub(board, tracker, possibles, 0, 0, x, y)
+            elif y >= 3 and y <= 5:
+                tracker = self.solveSub(board, tracker, possibles, 0, 3, x, y)
+            elif y >= 6 and y <= 8:
+                tracker = self.solveSub(board, tracker, possibles, 0, 6, x, y)
+        elif x >= 3 and x<= 5:
+            if y >= 0 and y <= 2:
+                tracker = self.solveSub(board, tracker, possibles, 3, 0, x, y)
+            elif y >= 3 and y <= 5: 
+                tracker = self.solveSub(board, tracker, possibles, 3, 3, x, y)
+            elif y >= 6 and y <= 8: 
+                tracker = self.solveSub(board, tracker, possibles, 3, 6, x, y)
+        elif x >= 6 and x <= 8: 
+            if y>= 0 and y <= 2: 
+                tracker = self.solveSub(board, tracker, possibles, 6, 0, x, y)
+            elif y >= 3 and y <= 5:
+                tracker = self.solveSub(board, tracker, possibles, 6, 3, x, y)
+            elif y >= 6 and y <= 8:
+                tracker = self.solveSub(board, tracker, possibles, 6, 6, x, y)
+        
+        return tracker
+    
+    def solveSub(self, board, tracker, possibles, low_x, low_y, x, y):
+        for arr_y in range(low_y + 3):
+            for arr_x in range(low_x + 3):
+                if board[arr_y][arr_x] == "." and not (arr_y == y and arr_x == x):
+                    for i in possibles[(arr_y,arr_x)]:
+                        for z in range(len(tracker)):
+                            if i == tracker[z]:
+                                tracker.pop(z)
+                                break
+        
+        return tracker
