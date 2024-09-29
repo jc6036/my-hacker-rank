4aa736c98d253bda6ac86867c5b45f586b4bdedc
Jesse Cabell <jc6036@gmail.com>
2024-09-29 16:59:47 -0400

    Added on to it over the past couple hours

--- sudoku.py
+++ sudoku.py
@@ -8,11 +8,12 @@ class Solution:
         # For every empty cell, set empty cell flag and attempt to solve
         # Solve - Insert a number starting from 1 to 9, until we find a valid number
         # If we find a valid num, recurse call solve(index + 1) - if on last cell, return True
-        # If we find no valid num, return False - this will totally exit the recursion on a bad puzzle
+        # If we find no valid num, do nothing, allow iteration through range of 0-8, but return False if we run out of options (back on first solve index w no answer)
 
-
-        for y in range(9):
-            for x in range(9):
+        if self.solve(board, 0, -1) == False:
+            print("Error with given input.")
+        else:
+            print(board)
 
     def solve(self, board: List[List[str]], index, first_solve):
         if index <= 8:
@@ -20,15 +21,13 @@ class Solution:
             x = index
         else:
             y = y // 8
-            x = index - (y * 8)
+            x = index - (y * 8) - 1
 
-        empty_cell = False
         if board[y][x] == ".":
-            empty_cell = True
             valid = True
             if first_solve < 0:
                 first_solve = index
-            for i in range(9):
+            for i in range(1, 10):
                 # Check Column grouping
                 for check_x in range(9):
                     if board[y][check_x] == "{}".format(i):
@@ -42,15 +41,23 @@ class Solution:
                     for check_y in range(y // 3, ((y // 3) + 3)):
                         if board[check_y][check_x] == "{}".format(i):
                             valid = False
-        
-        if empty_cell == False:
-            solve(board, index + 1, first_solve)
-            
-        if valid == True:
-            if y = 8 and x = 8:
+
+                if valid == True:
+                    board[y][x] = "{}".format(i)  # Save our possily valid answer to the board
+
+                    if y == 8 and x == 8:  # We are at the end, time to exit recursion
+                        return True
+                    
+                    if self.solve(board, index + 1, first_solve) == True:  # Ensure we actually exit the recursion on solve
+                        return True
+
+                if valid == False and i == 9:
+                    board[y][x] = "."  # No valid answer, clear it
+
+                    if index == first_solve:  # No valid answer for first open cell, bad sudoku, kill it
+                        return False
+        else:  # Pre-solved cell, skip it
+            if y == 8 and x == 8:  # Unless we are at the end, in which case consider it done
                 return True
-
-            solve(board, index + 1, first_solve)
-
-        if valid == False:
-            return False
+            else:  # Otherwise yeah, skip
+                self.solve(board, index + 1, first_solve)
