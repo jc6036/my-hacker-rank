536053c0c48a66ac7a30d41585ff465a8bffa486
Jesse Cabell <jc6036@gmail.com>
2024-10-05 11:55:39 -0400

    Still not yet working, figuring out a solution

--- Leetcode/CSharp/sum-divisible-by-p-remainder.cs
+++ Leetcode/CSharp/sum-divisible-by-p-remainder.cs
@@ -17,7 +17,19 @@ public class Solution {
         // will have a divisible sum by removing the sub array. Answer is simple there, % is more expensive
         // than a subtract and compare, should make it faster in theory.
 
-        int[] prefixArr = CreatePrefixRemainderArray(nums, p);
+        prefixes prefixContainer = CreatePrefixes(nums, p);
+        int[] prefixArr = prefixContainer.prefixRemainders;
+        long[] prefixSums = prefixContainer.prefixSums;
+
+        String prefixRemOut = "";
+        String prefixSumOut = "";
+        for(int i = 0; i<prefixArr.Length;i++)
+        {
+            prefixRemOut = prefixRemOut + "," + prefixArr[i];
+            prefixSumOut = prefixSumOut + "," + prefixSums[i];
+        }
+        /*Console.WriteLine(prefixRemOut);
+        Console.WriteLine(prefixSumOut);*/
 
         if (prefixArr[prefixArr.Length - 1] == 0)
         {
@@ -29,12 +41,17 @@ public class Solution {
 
         while(arrWindow.end - arrWindow.start < nums.Length - 1)
         {
-            if(prefixArr[prefixArr.Length - 1] == SubArrayRemainderFromPrefix(prefixArr, arrWindow))
+            if(prefixArr[prefixArr.Length - 1] == SubArrayRemainderFromPrefix(prefixArr, arrWindow, p) ||
+               prefixArr[prefixArr.Length - 1] == SubArraySumFromPrefix(prefixSums, arrWindow, p))
             {
                 return (arrWindow.end - arrWindow.start) + 1;
             }
             else
             {
+                /*Console.WriteLine("Main Rem: {0}",prefixArr[prefixArr.Length-1]);
+                Console.WriteLine("Point A: {0}, Point B: {1}",arrWindow.start,arrWindow.end);
+                Console.WriteLine("Sub Remainder: {0}",SubArrayRemainderFromPrefix(prefixArr, arrWindow, p));
+                Console.WriteLine("Sub Sum: {0}",SubArraySumFromPrefix(prefixSums, arrWindow, p));*/
                 arrWindow = IncrementWindow(nums.Length, arrWindow);
             }
         }
@@ -42,27 +59,24 @@ public class Solution {
         return -1;
     }
 
-    public int[] CreatePrefixRemainderArray(int[] nums, int p)
+    public prefixes CreatePrefixes(int[] nums, int p)
     {
+        prefixes ret = new prefixes();
+        ret.prefixRemainders = new int[nums.Length];
+        ret.prefixSums = new long[nums.Length];
+
         long sum = 0;
-        int[] prefixes = new int[nums.Length];
         for(int i = 0; i < nums.Length; i++)
         {
             sum = sum + (long)nums[i];
-            if(sum < (long)p)
-            {
-                prefixes[i] = nums[i];
-            }
-            else
-            {
-                prefixes[i] = (int)(sum % (long)p);
-            }
+            ret.prefixSums[i] = sum;
+            ret.prefixRemainders[i] = (int)(sum % (long)p);
         }
 
-        return prefixes;
+        return ret;
     }
 
-    public int SubArrayRemainderFromPrefix(int[] prefixes, window arrWindow)
+    public int SubArrayRemainderFromPrefix(int[] prefixes, window arrWindow, int p)
     {
         if(arrWindow.start == 0)
         {
@@ -70,7 +84,19 @@ public class Solution {
         }
         else
         {
-            return prefixes[arrWindow.end] - prefixes[arrWindow.start - 1];
+            return (prefixes[arrWindow.end] + Math.Abs(p - prefixes[arrWindow.start - 1]));
+        }
+    }
+
+    public long SubArraySumFromPrefix(long[] prefixes, window arrWindow, int p)
+    {
+        if(arrWindow.start == 0)
+        {
+            return prefixes[arrWindow.end];
+        }
+        else
+        {
+            return (prefixes[arrWindow.end] - prefixes[arrWindow.start - 1]);
         }
     }
 
@@ -107,5 +133,11 @@ public class Solution {
 
         public int start;
         public int end;
+    }
+
+    public struct prefixes
+    {
+        public int[] prefixRemainders;
+        public long[] prefixSums;
     }
 }
