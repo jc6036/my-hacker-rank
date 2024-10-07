25fec45e2c6b7954e3b49ec5b3799c2def79c5fa
Jesse Cabell <jc6036@gmail.com>
2024-10-07 14:03:04 -0400

    Final optimization to beat 90%

--- Leetcode/CSharp/merge-strings-alternately-short.cs
+++ Leetcode/CSharp/merge-strings-alternately-short.cs
@@ -137,3 +137,56 @@ public class Solution {
         return new String(output, 0, word1.Length + word2.Length);
     }
 }
+
+// My final optimization that beat 90% runtime
+public class Solution {
+    unsafe public string MergeAlternately(string word1, string word2) 
+    {
+        StringBuilder output = new StringBuilder();
+        byte[] indices = [0,0,0,1]; // Slight speed boost by using an array instead of individual vars
+        byte word1Length = (byte)word1.Length;
+        byte word2Length = (byte)word2.Length;
+        /*int bufIndex = 0;
+        int greaterLength = 0;
+        int shorterLength = 0;
+        int longerWord = 1;*/
+
+        if(word1Length >= word2Length)
+        {
+            indices[1] = word1Length;
+            indices[2] = word2Length;
+        }
+        else
+        {
+            indices[1] = word2Length;
+            indices[2] = word1Length;
+            indices[3] = 2;
+        }
+
+        for(int i = 0; i < indices[2]; i++)
+        {
+            output.Append(word1[i]);
+            output.Append(word2[i]);
+        }
+
+        if(indices[1] != indices[2])
+        {
+            if(indices[3] == 1)
+            {
+                for(int i = indices[2]; i < indices[1]; i++)
+                {
+                    output.Append(word1[i]);
+                }
+            }
+            else
+            {
+                for(int i = indices[2]; i < indices[1]; i++)
+                {
+                    output.Append(word2[i]);
+                }
+            }
+        }
+
+        return output.ToString();
+    }
+}
