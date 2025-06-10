121b619dae161a45a36589a581a774e0f39a32b9
Jesse Cabell <jc6036@gmail.com>
2025-06-10 15:47:02 -0400

    Python weighted graph implementation

---
+++ PythonWeightedGraph/graphnode.py
@@ -0,0 +1,6 @@
+class graphnode:
+    def __init__(self, key, edges):
+        self.key = key
+        self.edges = edges
+    
+    
---
+++ PythonWeightedGraph/mygraph.py
@@ -0,0 +1,66 @@
+class mygraph:
+    def __init__(self):
+        self.nodes = []
+
+    def add(self, newnode):
+        self.nodes.append(newnode)
+    
+    def remove(self, key):
+        self.nodes.remove(key)
+        self.clean_edges(key)
+    
+    def clean_edges(self, key):
+        for node in self.nodes:
+            weight = 0
+            for edge in node.edges:
+                if edge[0] == key:
+                    weight = edge[1]
+                    break
+            
+            node.edges.remove((key, weight))
+            break
+    
+    def dfsearch(self, key):
+        print("df search")
+        queue = []
+        visited = []
+
+        for node in self.nodes:
+            if node.key == key:
+                queue.insert(0, node)
+                break
+
+        while len(queue) > 0:
+            current = queue.pop(0)
+            if current not in visited:
+                print(current.key)
+                visited.append(current)
+                for edge in current.edges:
+                    for node in self.nodes:
+                        if node.key == edge[0]:
+                            queue.insert(0, node)
+                            break
+    
+    def bfsearch(self, key):
+        print("bf search")
+        queue = []
+        visited = []
+
+        for node in self.nodes:
+            if node.key == key:
+                queue.append(node)
+                break
+        
+        while len(queue) > 0:
+            current = queue.pop(0)
+            if current not in visited:
+                print(current.key)
+                visited.append(current)
+                for edge in current.edges:
+                    for node in self.nodes:
+                        if node.key == edge[0]:
+                            queue.append(node)
+                            break
+    
+    def dijkstra(self, key):
+        
